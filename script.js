1e5a15c08c67ea6cfec4063aec2c2bfadbba8315
Andrew Leck <Aleck360@gmail.com>
2022-05-13 17:57:57 -0500

    append info to the DOM

--- script.js
+++ script.js
@@ -2,6 +2,32 @@ console.log('Hello world');
 $(document).ready(readNow);
 
 function readNow(){
-    console.log('Hello world');
+$('.button').on('click', submitButton,);
+
+}
+
+
+function submitButton(){
+let firstName = $('.firstName').val();
+let lastName = $('.lastName').val();
+let ID = $('.Id').val();
+let Title = $('.title').val();
+let annualSalary = $('.annualSalary').val();
+
+$('.tableBody').append(`
+<tr>
+    <td>${firstName}</td>
+     <td>${lastName}</td>
+     <td> ${ID}</td>
+    <td>${Title}</td>
+    <td>${annualSalary}</td>
+</tr>
+`)
+
+$('.firstName').val(' ');
+$('.lastName').val(' ');
+$('.Id').val(' ');
+$('.title').val(' ');
+$('.annualSalary').val(' ');
 
 }
